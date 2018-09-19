b3e75e977ad13940c200106642bd760d6d0337f0
NilminiDharmakeerthi94 <nilmini.dharmakeerthi@gmail.com>
2018-09-19 19:25:04 +0530

    AJAX

---
+++ content.php
@@ -0,0 +1,4 @@
+<h2>
+	sample test 
+</h2>
+<p>sample paragraph</p>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JqUERY AJAX load method</title>
+</head>
+<body>
+<h1>example 1</h1>
+<button>
+	Loard method
+	</button>
+	<div class="content">
+
+
+	</div>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<script>
+		
+
+$(document).ready(function(){
+$('button').click(function(){
+	$('.content').load('content.php h2');
+
+});
+});
+
+	</script>
+</body>
+</html>
